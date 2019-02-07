dde9ccd07adb3e48f6aad7ff52e74d3df9f7dfe5
Crone1 <34443838+Crone1@users.noreply.github.com>
2019-02-07 11:24:40 +0000

    Create Nums_one_to_ten.java

---
+++ College/Week 2/For_Loops/Nums_one_to_ten.java
@@ -0,0 +1,11 @@
+public class OneToTen
+{
+    public static void main(String [] args)
+    {
+        // Use a for loop to print out the numbers.
+        int i;
+        for(i = 1; i < 11; i = i + 1)
+            System.out.print(i + " ");
+        System.out.println();
+    }
+}
