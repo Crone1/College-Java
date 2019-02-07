885e001ed0bf82e7bd1b7df4c3934219946cbed4
Crone1 <34443838+Crone1@users.noreply.github.com>
2019-02-07 10:57:53 +0000

    Create Even_or_odd.java

---
+++ College/Week 1/Even_or_odd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EvenOdd
+{
+    // Fill in the main method
+    public static void main(String [] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int x = in.nextInt();
+        if (x % 2 == 0)
+            System.out.println(x + " is even.");
+        else
+            System.out.println(x + " is odd.");
+    }   
+}
