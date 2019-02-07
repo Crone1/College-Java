c52754a926db6634450c31fd10d7445f313dcba1
Crone1 <34443838+Crone1@users.noreply.github.com>
2019-02-07 10:54:55 +0000

    Create Reduced_string.java

---
+++ College/Week 1/Reduced_string.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReducedString
+{
+    public static void main(String [] args)
+    {
+        // Create a scanner object
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Enter an integer and a string: ");
+        // Read in the string
+        int position = in.nextInt();
+        String word = in.next();
+
+        // work out what to print
+        System.out.println(word.substring(0,position) + word.substring(position + 1));
+    }
+}
