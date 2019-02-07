6f20399602cda968b79dbdd6f6f72b8aaf43051a
Crone1 <34443838+Crone1@users.noreply.github.com>
2019-02-07 11:20:29 +0000

    Create Nums_one_to_ten.java

---
+++ College/Week 2/While_Loops/Nums_one_to_ten.java
@@ -0,0 +1,15 @@
+public class OneToTen
+{
+    public static void main(String [] args)
+    {
+        int i = 1;
+        // Use a loop to print out the numbers
+        while (i < 11)
+            {
+            System.out.print(i + " ");
+            i = i + 1;
+            }
+       
+        System.out.println(); // Finish with a new line.
+    }
+}
