60c4a4e24afb421118565502a95d001f87b25984
Crone1 <34443838+Crone1@users.noreply.github.com>
2019-02-08 16:18:05 +0000

    Create Find_largest_int.java

---
+++ College/Week 2/Methods/Find_largest_int.java
@@ -0,0 +1,14 @@
+public class Test
+{
+    public static int largest(int a, int b, int c)
+    {
+        if (a > b && a > c)
+            return a;
+            
+        if (b > a && b > c)
+            return b;
+            
+        else // (c>a && c>b)
+            return c;    
+    }
+}
