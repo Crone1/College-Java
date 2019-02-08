d07380a164a6bce88e77dcc69eec5742401123f9
Crone1 <34443838+Crone1@users.noreply.github.com>
2019-02-08 15:53:45 +0000

    Create Nums_five_to_twenty.java

---
+++ College/Week 2/For_Loops/Nums_five_to_twenty.java
@@ -0,0 +1,10 @@
+public class FiveToTwenty
+{
+    public static void main(String [] args)
+    {
+        int i;
+        for(i = 5; i < 21; i = i + 1)
+            System.out.print(i + " ");
+        System.out.println();
+    }
+}
