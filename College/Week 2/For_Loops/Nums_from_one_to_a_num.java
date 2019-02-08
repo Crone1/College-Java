00f84492870369a98f018ea3a95ae13d6a66a240
Crone1 <34443838+Crone1@users.noreply.github.com>
2019-02-08 15:55:07 +0000

    Create Nums_from_one_to_a_num.java

---
+++ College/Week 2/For_Loops/Nums_from_one_to_a_num.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class OneToNum
+{
+    public static void main(String [] args)
+    {
+        System.out.print("Enter a number: ");
+        Scanner in = new Scanner(System.in);
+        
+        double num = in.nextDouble();
+                
+        // Use a for loop to print out the numbers.
+        int i;
+        for(i = 1; i < num + 1; i = i + 1)
+            System.out.print(i + " ");
+            
+        System.out.println();
+    }
+}
