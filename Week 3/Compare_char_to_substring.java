cc0054b5240e76f68c3e8529b099efb17a66abbc
Crone1 <34443838+Crone1@users.noreply.github.com>
2019-03-18 15:46:29 +0000

    Create Compare_char_to_substring.java

---
+++ Week 3/Compare_char_to_substring.java
@@ -0,0 +1,8 @@
+public class Word
+{
+    public static boolean isFirstLetter(String word, char letter)
+    {
+        String temp = Character.toString(letter);
+        return (word.substring(0,1).equals(temp));
+    }
+}
