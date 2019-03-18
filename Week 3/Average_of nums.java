379bd21864b220e15f2af6735f3efcb829adfa27
Crone1 <34443838+Crone1@users.noreply.github.com>
2019-03-18 15:37:25 +0000

    Create Average_of nums.java

---
+++ Week 3/Average_of nums.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class OnlyAverage
+{
+    public static void main(String [] args)
+    {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("How many numbers: ");
+        int num = in.nextInt();
+        
+        System.out.print("Enter " + num + " numbers: ");
+        
+        double sum = 0;
+        int i = 0;
+        while (i < num)
+        {
+            sum = sum + in.nextDouble();
+            i++;
+        }
+
+         double average = (sum/num);
+        System.out.println("The average is " + average);
+    }
+}
