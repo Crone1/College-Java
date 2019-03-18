8fe8afaa75b3d357fae174daf3543dc42ea111cd
Crone1 <34443838+Crone1@users.noreply.github.com>
2019-03-18 15:56:58 +0000

    Create miniHangman.java

---
+++ Week 3/miniHangman.java
@@ -0,0 +1,35 @@
+public class miniHangman
+{
+    public static boolean containsLetter(String word, char letter)
+    {
+        String temp = Character.toString(letter);
+        int i;
+        for (i = 0; i < word.length(); i = i + 1)
+        {
+            if (word.substring(i,i + 1).equals(temp))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static String showLetter(String word, char guess)
+    {
+        int i = 0;
+        String s = "";
+        for (i = 0; i < word.length(); i++)
+        {
+            if (containsLetter(word.substring(i, i + 1), guess))
+            {
+                String c = Character.toString(guess);
+                s = s + c;
+            }
+            else
+            {
+                s = s + "_";
+            }
+        }
+        return s;
+    }
+}
