2aff0411307f8669825b313a2ad6ae1eab4e1821
Crone1 <34443838+Crone1@users.noreply.github.com>
2019-03-18 17:29:56 +0000

    Update printStudentsArray.java

--- College/Week 4/Classes & Arrays/printStudentsArray.java
+++ College/Week 4/Classes & Arrays/printStudentsArray.java
@@ -1,8 +1,21 @@
 public class printStudentsArray
 {
-   public static void print(Student [] student)
+   public static void printName(Student [] student)
    {
       for(int i = 0; i < student.length; i++)
          System.out.println(student[i].name);
    }
+   
+   public static void printStudent(Student [] student)
+   {
+      for(int i = 0; i < student.length; i++)
+         System.out.println(student[i].mark + " (" + student[i].name + ")");
+   }
+  
+   public static void printMark(Student [] student)
+   {
+      for(int i = 0; i < student.length; i++)
+         System.out.println(student[i].mark);
+   }
+
 }
