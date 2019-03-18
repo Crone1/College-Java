f6bbaa126a747499dac018e3c567f3dbebabb00f
Crone1 <34443838+Crone1@users.noreply.github.com>
2019-03-18 19:21:40 +0000

    Create GenericMaximum.java

---
+++ College/Week 6/Generics/GenericMaximum.java
@@ -0,0 +1,15 @@
+public class GenericMaximum
+{
+    public static <T extends Order> T max(T a, T b)
+    {
+        if (a.greaterThan(b))
+        {
+            return a;
+        }
+    
+        else
+        {
+            return b;
+        }
+    }
+}
