231e618bf7a0bdaa9dd644605364545f121b9d34
Crone1 <34443838+Crone1@users.noreply.github.com>
2019-03-18 15:42:22 +0000

    Create Count_evens.java

---
+++ Week 3/Count_evens.java
@@ -0,0 +1,16 @@
+public class Test
+{
+    public static int countEven(int [] array)
+    {
+        int i;
+        int total = 0;
+        for (i = 0; i < array.length; i = i + 1)
+        {
+            if (array[i] % 2 == 0)
+            {
+            total = total + 1;
+            }
+        }
+        return total;
+    }
+}
