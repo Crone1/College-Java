3b901bb35e7e5560a85e074bfe3a6a074a34c1b2
Crone1 <34443838+Crone1@users.noreply.github.com>
2019-03-19 19:51:24 +0000

    Create Animal.java

---
+++ College/Week 7/Inheritance/Animal.java
@@ -0,0 +1,15 @@
+public class Animal
+{
+    String name;
+    
+    public Animal(String n)
+    {
+        name = n;
+    }
+    
+    public String greeting()
+    {
+        return ("Hello, my name is " + this.name);
+    }
+    
+}
